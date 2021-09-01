4ed11e205704941b5933c87b4a1307b374d68e83
Renato <renatoviei@gmail.com>
2021-09-01 16:55:27 -0300

    Criação de projeto console App de Agência Bancária
    Foram criadas classes básicas, construtores e validações de construtores.

---
+++ AgenciaBancaria/AgenciaBancaria.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("Rua Araguaia", "11111222", "Recife", "Pernambuco");
+                Cliente cliente = new Cliente("Renato", "12345678900", "7654321", endereco);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ AgenciaBancaria/AgenciaBancaria.Dominio/Validacoes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public static class Validacoes
+    {
+        //Exemplo de método de extensão
+        public static string ValidaStringVazia(this string texto)
+        {
+            return string.IsNullOrWhiteSpace(texto) ?
+                            throw new Exception("Propriedade deve estar preenchida")
+                            : texto;
+        }
+    }
+}
